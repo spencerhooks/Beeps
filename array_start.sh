851d481d5e22f320407e1eeadca6816938288edf
Spencer Hooks <spencerhooks@gmail.com>
2019-01-26 17:52:05 -0800

    Create array_start.sh

---
+++ array_start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Plays the Mario victory song
+# Original source: https://blog.dhampir.no/content/fun-with-beep
+# I found via https://github.com/ShaneMcC/beeps/ 
+
+beep -f 130 -l 100 -n -f 262 -l 100 -n -f 330 -l 100 -n -f 392 -l 100 -n -f 523 -l 100 -n -f 660 -l 100 -n -f 784 -l 300 -n -f 660 -l 300 -n -f 146 -l 100 -n -f 262 -l 100 -n -f 311 -l 100 -n -f 415 -l 100 -n -f 523 -l 100 -n -f 622 -l 100 -n -f 831 -l 300 -n -f 622 -l 300 -n -f 155 -l 100 -n -f 294 -l 100 -n -f 349 -l 100 -n -f 466 -l 100 -n -f 588 -l 100 -n -f 699 -l 100 -n -f 933 -l 300 -n -f 933 -l 100 -n -f 933 -l 100 -n -f 933 -l 100 -n -f 1047 -l 400
