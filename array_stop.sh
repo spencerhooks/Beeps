f5f326a26cd630fcbc1035d5d0da6f36b86788b0
Spencer Hooks <spencerhooks@gmail.com>
2019-01-26 17:53:57 -0800

    Create array_stop.sh

---
+++ array_stop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Plays the beginning of the Star Wars Imperial March
+# Original source: # http://lime-technology.com/forum/index.php?topic=10749.20;wap2
+# I found via: https://github.com/ShaneMcC/beeps/
+
+beep -l 350 -f 392 -D 100 -n -l 350 -f 392 -D 100 -n -l 350 -f 392 -D 100 -n -l 250 -f 311.1 -D 100 -n -l 25 -f 466.2 -D 100 -n -l 350 -f 392 -D 100 -n -l 250 -f 311.1 -D 100 -n -l 25 -f 466.2 -D 100 -n -l 700 -f 392 -D 100 -n -l 350 -f 587.32 -D 100 -n -l 350 -f 587.32 -D 100 -n -l 350 -f 587.32 -D 100 -n -l 250 -f 622.26 -D 100 -n -l 25 -f 466.2 -D 100 -n -l 350 -f 369.99 -D 100 -n -l 250 -f 311.1 -D 100 -n -l 25 -f 466.2 -D 100 -n -l 700 -f 392 -D 100
